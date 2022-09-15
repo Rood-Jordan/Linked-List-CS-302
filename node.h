5d7fed462bfee3121adae181bdfc70be0f45c1e0
Jordan Rood <70539336+Rood-Jordan@users.noreply.github.com>
2022-09-15 15:45:40 -0700

    Update node.h

---
+++ node.h
@@ -0,0 +1,22 @@
+#ifndef NODE_H
+#define NODE_H
+
+using namespace std;
+
+template<class ItemType>
+class Node {
+    ItemType item;
+    Node<ItemType>* next;
+
+public:
+    Node();
+    Node(const ItemType& anItem);
+    Node(const ItemType& anItem, Node<ItemType>* nextNodePtr);
+    void setItem(const ItemType& anItem);
+    void setNext(Node<ItemType>* nextNodePtr);
+    ItemType getItem() const;
+    Node<ItemType>* getNext() const;
+
+};
+#include "node.cpp"
+#endif

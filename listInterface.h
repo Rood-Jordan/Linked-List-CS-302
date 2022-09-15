97d7ec43a37bb80b64451811abb577b7c764d830
Jordan Rood <70539336+Rood-Jordan@users.noreply.github.com>
2022-09-15 15:44:31 -0700

    Update listInterface.h

---
+++ listInterface.h
@@ -0,0 +1,18 @@
+#ifndef LIST_INTERFACE_H
+#define LIST_INTERFACE_H
+
+using namespace std;
+
+template<class ItemType>
+class listInterface {
+public:
+    virtual bool isEmpty() const = 0;
+    virtual int getLength() const = 0;
+    virtual bool insert(int newPosition, const ItemType& newEntry) = 0;
+    virtual bool remove(int position) = 0;
+    virtual void clear() = 0;
+    virtual ItemType getEntry(int position) const = 0;
+    virtual ItemType replace(int position, const ItemType& newEntry) = 0;
+    virtual ~listInterface() {}
+};
+#endif
